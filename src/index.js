6ec1ccfd17e6d1237717601fc3eaad71ee7f23fa
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2024-02-12 15:51:59 -0500

    Setup basic fetch and get some data

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 import "./style.css";
 
+async function getWeatherData(input) {
+    try {
+        // let location = "london";
+        const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=2131437186b046c39d7223234240802&q=${input}&aqi=no`, {mode: 'cors'})
+        const getData = await response.json();
+        
+        let location = `${getData.location.name}, ${getData.location.country}`;
 
+        let todayCondition = `${getData.current.condition.text}`;
+
+        console.log(getData);
+        console.log(location);
+        console.log(todayCondition);
+    } catch(error) {
+        console.log(error);
+    }
+
+}
+
+getWeatherData('toronto');
+
+
+// data needed
+// 3 day forecast
+// week day
+// temperature
+// weather description
+// humidity
+// city and country name
