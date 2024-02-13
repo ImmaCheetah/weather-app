06b946fc36dfb1143d72f35186b8699d16888487
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2024-02-13 15:46:52 -0500

    Create function to show basic location and condition

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import "./style.css";
 
-export default async function getWeatherData(input) {
+async function getWeatherData(input) {
     try {
         const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=2131437186b046c39d7223234240802&q=${input}&aqi=no`, {mode: 'cors'})
         const getData = await response.json();
 
-        getTodayCondition(getData);
-        getLocation(getData);
+        // getTodayCondition(getData);
+        // getLocation(getData);
         console.log(getData);
         return getData;
 
@@ -40,14 +40,27 @@ function getLocation(data) {
     return myLocation;
 }
 
+async function displayData(input) {
+    const content = document.getElementById('content');
+    const weatherData = await getWeatherData(input);
+
+    const location = getLocation(weatherData);
+    const condition = await getTodayCondition(weatherData);
+    
+    console.log(location.country);
+    content.textContent = `This is in ${location.country}, and the weather be ${condition.conditionText}`;
+}
+
 const searchField = document.querySelector('.search-field');
 const searchBtn = document.querySelector('.search-btn');
 
 searchBtn.addEventListener('click', (e)=>{
     e.preventDefault();
-    console.log();
-    getWeatherData(searchField.value.toString());
+    // console.log();
+    // getWeatherData();
+    displayData(searchField.value.toString());
 })
+
 
 // getWeatherData('toronto');
 
